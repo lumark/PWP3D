837c2f8961aee7e02f4ec3ff840904325512e2b9
luma <lu.ma@colorado.edu>
2014-10-26 13:47:51 -0600

    prepare init args with command line

--- Application/PerseusLibTest.cpp
+++ Application/PerseusLibTest.cpp
@@ -7,6 +7,16 @@ using namespace Perseus::Utils;
 
 int main(void)
 {
+  std::string sModelPath = "/Users/luma/Code/Luma/PWP3D/Files/Models/Renderer/long.obj";
+  //  std::string sModelPath = "/Users/luma/Code/DataSet/Mesh/BlueCar.obj";
+  //  std::string sModelPath = "/Users/luma/Code/DataSet/Mesh/RedCan.obj";
+
+  std::string sSrcImage = "/Users/luma/Code/Luma/PWP3D/Files/Images/Red.png";
+  std::string sCameraMatrix = "/Users/luma/Code/Luma/PWP3D/Files/CameraCalibration/900nc.cal";
+  std::string sTargetMask = "/Users/luma/Code/Luma/PWP3D/Files/Masks/480p_All_VideoMask.png";
+  std::string sHistSrc = "/Users/luma/Code/Luma/PWP3D/Files/Masks/Red_Source.png";
+  std::string sHistMask = "/Users/luma/Code/Luma/PWP3D/Files/Masks/Red_Mask.png";
+
 	char str[100];
 	int i;
 
@@ -17,29 +27,33 @@ int main(void)
 	Timer t;
 
 	//result visualisation
-	ImageUChar4* result = new ImageUChar4(width, height);
+  ImageUChar4* ResultImage = new ImageUChar4(width, height);
 
 	//input image
 	//camera = 24 bit colour rgb
 	ImageUChar4* camera = new ImageUChar4(width, height);
-  ImageUtils::Instance()->LoadImageFromFile(camera, "/Users/luma/Code/Luma/PWP3D/Files/Images/Red.png");
+  ImageUtils::Instance()->LoadImageFromFile(camera, (char*)sSrcImage.c_str());
 
 	//objects allocation + initialisation: 3d model in obj required
 	Object3D **objects = new Object3D*[objectCount];
-  objects[objectIdx] = new Object3D(objectId, viewCount, "/Users/luma/Code/Luma/PWP3D/Files/Models/Renderer/long.obj", width, height);
+
+  std::cout<<"\n==[APP] Init Model =="<<std::endl;
+  objects[objectIdx] = new Object3D(objectId, viewCount, (char*)sModelPath.c_str(), width, height);
 
 	//views allocation + initialisation: camera calibration (artoolkit format) required
+  std::cout<<"\n==[APP] Init CameraMatrix =="<<std::endl;
 	View3D **views = new View3D*[viewCount];
-  views[viewIdx] = new View3D(0, "/Users/luma/Code/Luma/PWP3D/Files/CameraCalibration/900nc.cal", width, height);
+  views[viewIdx] = new View3D(0, (char*)sCameraMatrix.c_str(), width, height);
 
 
 	//histogram initialisation
 	//source = 24 bit colour rgb
 	//mask = 24 bit black/white png - white represents object
 	//videoMask = 24 bit black/white png - white represents parts of the image that are usable
-  ImageUtils::Instance()->LoadImageFromFile(views[viewIdx]->videoMask, "/Users/luma/Code/Luma/PWP3D/Files/Masks/480p_All_VideoMask.png");
-  ImageUtils::Instance()->LoadImageFromFile(objects[objectIdx]->histSources[viewIdx], "/Users/luma/Code/Luma/PWP3D/Files/Masks/Red_Source.png");
-  ImageUtils::Instance()->LoadImageFromFile(objects[objectIdx]->histMasks[viewIdx], "/Users/luma/Code/Luma/PWP3D/Files/Masks/Red_Mask.png", objectIdx+1);
+  std::cout<<"\n==[APP] Init Target ROI =="<<std::endl;
+  ImageUtils::Instance()->LoadImageFromFile(views[viewIdx]->videoMask,(char*)sTargetMask.c_str());
+  ImageUtils::Instance()->LoadImageFromFile(objects[objectIdx]->histSources[viewIdx], (char*)sHistSrc.c_str());
+  ImageUtils::Instance()->LoadImageFromFile(objects[objectIdx]->histMasks[viewIdx], (char*)sHistMask.c_str(), objectIdx+1);
 	HistogramEngine::Instance()->UpdateVarBinHistogram(objects[objectIdx], views[viewIdx], objects[objectIdx]->histSources[viewIdx], 
 		objects[objectIdx]->histMasks[viewIdx], views[viewIdx]->videoMask);
 
@@ -66,10 +80,14 @@ int main(void)
 	//register camera image with main engine
 	OptimisationEngine::Instance()->RegisterViewImage(views[viewIdx], camera);
 
-  VisualisationEngine::Instance()->GetImage(result, GETIMAGE_PROXIMITY, objects[objectIdx], views[viewIdx], objects[objectIdx]->initialPose[viewIdx]);
-  cv::Mat ResultMat(height,width,CV_8UC4, result->pixels);
+  std::cout<<"\n==[APP] Rendering object initial pose.. =="<<std::endl;
+  VisualisationEngine::Instance()->GetImage(ResultImage, GETIMAGE_PROXIMITY, objects[objectIdx], views[viewIdx], objects[objectIdx]->initialPose[viewIdx]);
+
+  cv::Mat ResultMat(height,width,CV_8UC4, ResultImage->pixels);
   cv::imshow("initial pose", ResultMat);
-  cv::waitKey(1000);
+  cv::waitKey(2000);
+
+  std::cout<<"[App] Finish Rendered object initial pose."<<std::endl;
 
   for (i=0; i<1; i++)
   {
@@ -103,11 +121,11 @@ int main(void)
     t.check("Iteration");
 
     //result plot
-    VisualisationEngine::Instance()->GetImage(result, GETIMAGE_PROXIMITY, objects[objectIdx], views[viewIdx], objects[objectIdx]->pose[viewIdx]);
+    VisualisationEngine::Instance()->GetImage(ResultImage, GETIMAGE_PROXIMITY, objects[objectIdx], views[viewIdx], objects[objectIdx]->pose[viewIdx]);
 
     //result save to file
     //    ImageUtils::Instance()->SaveImageToFile(result, str);
-    cv::Mat ResultMat(height,width,CV_8UC4, result->pixels);
+    cv::Mat ResultMat(height,width,CV_8UC4, ResultImage->pixels);
     cv::imshow("result", ResultMat);
     cv::waitKey(10000);
 
@@ -121,8 +139,8 @@ int main(void)
 
   //posteriors plot
   sprintf(str, "/Users/luma/Code/Luma/PWP3D/iles/Results/posteriors.png");
-  VisualisationEngine::Instance()->GetImage(result, GETIMAGE_POSTERIORS, objects[objectIdx], views[viewIdx], objects[objectIdx]->pose[viewIdx]);
-  ImageUtils::Instance()->SaveImageToFile(result, str);
+  VisualisationEngine::Instance()->GetImage(ResultImage, GETIMAGE_POSTERIORS, objects[objectIdx], views[viewIdx], objects[objectIdx]->pose[viewIdx]);
+  ImageUtils::Instance()->SaveImageToFile(ResultImage, str);
 
 	//primary engine destructor
 	OptimisationEngine::Instance()->Shutdown();
@@ -133,7 +151,7 @@ int main(void)
 	for (i = 0; i<viewCount; i++) delete views[i];
 	delete views;
 
-	delete result;
+  delete ResultImage;
 
   std::cout<<"Exit pwp3D app successfully."<<std::endl;
 
